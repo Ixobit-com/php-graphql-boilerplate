94fbfb2bf63dbc57494c63001c776a7744d27e17
valery <valery.makarenko@sibers.com>
2024-07-23 18:31:39 +0700

    improve documentation

--- .docker/documentation.sh
+++ .docker/documentation.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
-## Output styles
-txtyel=$(tput setaf 3)    # Yellow
-txtgrn=$(tput setaf 2)    # Green
-txtred=$(tput setaf 1)    # Red
-txtrst=$(tput sgr0)       # Text reset
+
+## Generate documentation in ./public/documentation folder broken by schemas
 
 ## Get configuration parameters
 source .env
 
-declare -a arr=("auth" "user")
+declare -a arr=("auth" "user") ## API Schemas
+
+echo "" > ../public/documentation/index.html
+cat << EOF > ../public/documentation/index.html
+<!doctype html>
+<html>
+    <meta charset=utf-8>
+    <title>API Documentation</title>
+    <body>
+        <h1>API Documentation</h1>
+        <ul>
+EOF
+
 for schema in "${arr[@]}"
 do
     docker compose run php-fpm ./bin/console graphql:dump-schema --schema=$schema --classic --format=graphql --file=./var/$schema.gql
     docker compose run nodejs npx spectaql --schema-file ./var/$schema.gql --target-dir ./public/documentation/$schema/ -c ./config/packages/spectacul.yml
+    cat << EOF >> ../public/documentation/index.html
+        <li><strong><a href='./$schema/index.html'>$schema</a></strong></li>
+EOF
 done
+
+cat << EOF >> ../public/documentation/index.html
+        </ul>
+    </body>
+</html>
+EOF
 
 exit 0;
